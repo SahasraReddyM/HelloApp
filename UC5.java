3e80bbefaad0ac21adf18e635e0d6f222bd8f8c0
SahasraReddyM <mro476@srmist.edu.in>
2026-03-28 09:50:34 +0530

    UC5: Enhanced for loop greeting

---
+++ UC5.java
@@ -0,0 +1,14 @@
+public class UC5{
+    public static void main(String[] args) {
+
+        // Check if no arguments are passed
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for loop (for-each loop)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
+}
